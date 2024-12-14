db25543e408a0323dd34da73ef88a4fe27f71e0c
nitesh <niteshtirkey4@gmail.com>
2024-12-14 12:58:19 +0530

    first java

---
+++ 1_Setup/Hello.java
@@ -0,0 +1,12 @@
+class Hello{
+
+    public static void main(String a[]){
+    System.out.println( 22+12);
+    int num1 = 10;
+    int num2 = 30;
+    int results = num1 * num2;
+
+    System.out.println(results);
+    }
+
+}
