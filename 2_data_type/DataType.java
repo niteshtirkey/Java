ea316a41e5d9a270311d9c80c56eda0b50d7dc14
nitesh <niteshtirkey4@gmail.com>
2024-12-14 14:11:28 +0530

    Data Type

---
+++ 2_data_type/DataType.java
@@ -0,0 +1,26 @@
+public class DataType {
+
+    public static void main(String[]args){
+
+       int num1 = 20;
+       byte by = 123;
+       short sh = 212;
+       long l = 11122l;
+
+       float f = 1.3f;
+       double d = 1.2;
+
+       char k = 'k';
+
+       boolean b = true;
+
+    }
+}
+
+// Data Type
+
+// String - stores text, such as "Hello". String values are surrounded by double quotes
+// int - stores integers (whole numbers), without decimals, such as 123 or -123 -- byte=1, int=4 byte, long=8 byte, short = 2 byte,
+// float - stores floating point numbers, with decimals, such as 19.99 or -19.99 -- float - 4 byte , double-8byte
+// char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+// boolean - stores values with two states: true or false
