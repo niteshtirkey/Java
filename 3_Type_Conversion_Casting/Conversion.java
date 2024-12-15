da67383310e006d67d916b145dfe21edb5d2e5c5
nitesh <niteshtirkey4@gmail.com>
2024-12-15 09:49:25 +0530

    Conversion

---
+++ 3_Type_Conversion_Casting/Conversion.java
@@ -0,0 +1,17 @@
+
+public class Conversion {
+    public static void main(String args[]){
+        // byte b = 127;
+        int a =  122; // assign byte to int
+        byte k = (byte)a; // not assign int to byte.only (byte) can work
+
+        float f = 3.4f;
+        int t = (int) f;
+
+        byte num1 = 22;
+        byte num2 = 33;
+
+        int results = num1 + num2;
+        System.out.println(results);
+    }
+}
