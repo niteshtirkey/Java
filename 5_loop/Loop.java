109705c404edd66960c8646dc937b2611a21a524
nitesh <niteshtirkey4@gmail.com>
2024-12-25 16:06:48 +0530

    loop

---
+++ 5_loop/Loop.java
@@ -0,0 +1,21 @@
+
+// while loop
+public class Loop {
+    public static void main(String a[]){
+        {
+            int i = 1;
+            while(i<=4)
+            {
+                    System.out.println("Hii" + i);
+                    int j = 1;
+                    while(j<=3){
+                        System.out.println("Hello" +j );
+                        j++;
+                    }
+                ++i;
+            }
+            System.out.println("bye" + i);
+        }
+    }
+    
+}  
---
+++ 5_loop/dowhile.java
@@ -0,0 +1,28 @@
+
+// while loop
+public class dowhile {
+    public static void main(String a[]){
+        {
+            int i = 5;
+            do
+            {
+                System.out.println("Hii" + i);
+                ++i;
+            }while(i<=4);
+        }
+        {   
+            for(int i=3; i>=1; i--){
+                System.out.println("Nitesh" + i);
+            }
+        }
+        {
+            for(int i=1; i<=5; i++){
+                System.out.println("Day" +i);
+                // for(int j=1; j<=9; j++){
+                //     System.out.println(" " + (j+8) + "-" + (j+9));
+                // }
+            }
+        }
+    }
+    
+}  
